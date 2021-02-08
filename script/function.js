cca7b20f1fa466e51b8b6d12759b7065b83b4380
ravigavde <nightr714@gmail.com>
2021-02-08 15:18:40 +0530

    initial commit

---
+++ script/function.js
@@ -0,0 +1,7 @@
+console.log("function file");
+function click()
+{
+    console.log("clicked");
+    let paragraph = document.getElementById("para");
+    paragraph.innerHTML = "Newly added lines" ;
+}
